54f6050e65dd9b3899cf2bd9df3665df11ac12da
Tim Ross <tim@timross.info>
2014-08-13 21:16:31 +1200

    Merged rate xml parser

--- Challenges/1/1-international-trade.playground/section-2.swift
+++ Challenges/1/1-international-trade.playground/section-2.swift
@@ -13,18 +13,6 @@ class Transaction {
     }
 }
 
-class Rate {
-    let fromCurrency: String
-    let toCurrency: String
-    let conversion: Double
-    
-    init(fromCurrency: String, toCurrency: String, conversion: NSDecimalNumber){
-        self.fromCurrency = fromCurrency
-        self.toCurrency = toCurrency
-        self.conversion = conversion
-    }
-}
-
 let bundle = NSBundle.mainBundle()
 
 let transPath = bundle.pathForResource("SAMPLE_TRANS", ofType: "csv")
@@ -40,7 +28,65 @@ for line in cvsRows {
     let transaction = Transaction(store: values[0], sku: values[1], amount: NSDecimalNumber(string: currencyAmount[0]), currency: currencyAmount[1])
     transactions.append(transaction)
 }
-transactions
+
+struct Rate {
+    var from: String = "",
+        to: String = "",
+        conversionRate: NSDecimalNumber = NSDecimalNumber()
+}
+
+class Rates : NSXMLParser, NSXMLParserDelegate {
+    var rates: [Rate]
+
+    var currentElement: String?,
+        currentRate: Rate?
+
+    init(fileName: String) {
+        rates = Array()
+
+        var error: NSError?;
+        var data = NSData(contentsOfFile: fileName,
+                                 options: .DataReadingMappedIfSafe,
+                                   error: &error)
+
+        super.init(data: data)
+
+        self.delegate = self;
+        self.parse()
+    }
+
+    func parser(parser: NSXMLParser!, didStartElement elementName: String!, namespaceURI: String!, qualifiedName: String!, attributes: [NSObject : AnyObject]!) {
+        currentElement = elementName
+
+        if elementName == "rate" {
+            currentRate = Rate()
+        }
+    }
+
+    func parser(parser: NSXMLParser!, foundCharacters string: String!) {
+        if let element = currentElement {
+            switch element {
+                case "from":
+                    currentRate!.from = string
+                case "to":
+                    currentRate!.to = string
+                case "conversion":
+                    currentRate!.conversionRate = NSDecimalNumber(string: string)
+                default:
+                    break
+            }
+        }
+    }
+
+    func parser(parser: NSXMLParser!, didEndElement elementName: String!, namespaceURI: String!, qualifiedName: String!) {
+        currentElement = nil
+        if elementName == "rate" {
+            rates.append(currentRate!)
+        }
+    }
+}
 
 let ratesPath = bundle.pathForResource("SAMPLE_RATES", ofType: "xml")
-let xmlString = String.stringWithContentsOfFile(ratesPath, encoding: NSUTF8StringEncoding, error: nil)
+let rates = Rates(fileName: ratesPath)
+rates.rates
+
